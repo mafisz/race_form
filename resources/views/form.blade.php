af36b022761c877c9025684c84b448d12c35dbe8
mafisz <mafisz@gmail.com>
2018-04-04 13:23:01 +0200

    main template

---
+++ resources/views/form.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header text-center bg-dark text-white"><h3>Forumlarz zgłoszeniowy</h3></div>
+
+                <div class="card-body">
+                    @include('messages')
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
