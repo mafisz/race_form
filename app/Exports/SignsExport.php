b95c7d3da02e573af9d3c172b38741d046c8db91
mafisz <mafisz@gmail.com>
2018-04-05 14:34:52 +0200

    custom sort

--- app/Exports/SignsExport.php
+++ app/Exports/SignsExport.php
@@ -11,6 +11,36 @@ class SignsExport implements FromView, ShouldAutoSize
    public function view(): View
     {
         $signs = Sign::orderBy('klasa', 'asc')->get();
+
+        $signs = $signs->sortBy(function ($sign, $key) {
+            switch ($sign['klasa']) {
+                case 'K4':
+                    return 1;
+                    break;
+                case 'K7':
+                    return 2;
+                    break;
+                case 'K3':
+                    return 3;
+                    break;
+                case 'K2':
+                    return 4;
+                    break;
+                case 'K1':
+                    return 5;
+                    break;
+                case 'K6':
+                    return 6;
+                    break;
+                case 'K5':
+                    return 7;
+                    break;
+                default:
+                    return 1;
+                    break;
+            }
+        });
+
         return view('exports.signs', [
             'signs' => $signs->all()
         ]);
